5c49367bcee8157d8067f8cb98a443679af942d8
kavikoglory23 <99467648+kavikoglory23@users.noreply.github.com>
2022-02-11 12:31:08 +0300

    Add files via upload

---
+++ cdv.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import streamlit as st
+st.header("'sample web app'")
+st.write("'my app'")
+celcius=st.slider('val')
+st.write(celcius,"fahrenheit",(celcius*9/5)+32)
+
+
+# In[ ]:
+
+
+import streamlit as st
+st.header("'sample web app'")
+st.write("'my app'")
+sphere=st.slider('vai')
+st.write('sphere',(4/3pir**3))
+
